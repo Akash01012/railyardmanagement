fa1a9620db0854cb084702115c18e270587af4ef
akash kumar <akash639673@gmail.com>
2025-12-31 16:10:29 +0530

    update to keep server alive

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ import subprocess
 
 st.set_page_config(page_title="🚆 RailYard Protocol Simulation", layout="wide")
 
-if st.experimental_get_query_params().get("ping"):
+if st.query_params.get("ping"):
     st.write("OK")
     st.stop()
-
 st.title("🚆 RailYard Protocol Simulation")
 
 st.write("Press the button below to run the RailYard simulation:")
